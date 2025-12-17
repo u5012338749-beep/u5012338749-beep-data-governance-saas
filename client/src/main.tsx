69382f76a965bc04bddeafc49b9d6875eea43bcf
u5012338749-beep <u5012338749@gmail.com>
2025-12-17 17:35:34 +0100

    Inject speed insights in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,6 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
